263c7af01626b055dea12c7aadf53018245b26a5
Laszlo Sajkiewicz <laszlo-94@hotmail.fr>
2015-07-21 09:37:48 +0200

    Create operation.c
    add addition, soustraction,multiplication , division and modulo function

---
+++ operation.c
@@ -0,0 +1,30 @@
+double	f_addition(double a, double b) 
+{
+double resultat;
+resulta = a + b;
+return (resultat);
+}
+double	f_soustraction(double a, double b) 
+{
+double resultat;
+resulta = a - b;
+return (resultat);
+}
+double	f_multipication(double a, double b)
+{
+double resultat;
+resulta = a * b;
+return (resultat);
+}
+double	f_division(double a, double b) 
+{
+double resultat;
+resulta = a / b;
+return (resultat);
+}
+int	f_modulo(int a, int b) 
+{
+int resultat;
+resulta = a % b;
+return (resultat);
+}
