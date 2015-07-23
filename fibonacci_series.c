a139d9db9ad91edd227dc64530a665d014cf4fc6
Laszlo Sajkiewicz <laszlo-94@hotmail.fr>
2015-07-23 08:39:52 +0200

    completed

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
 
+void main()
+{
+  int a,b,c,i,n;
+  cirscr();
+  a=0;
+  b=1;
+  
+  printf("\n Enter n for how many times generate series");
+  scanf("%d",&n);
+  printf("\n FIBONACCI SERIES \n");
+  printf("\t%d\t%d",a,b);
+  for(i=0,i<n,i++){
+    c=a+b;
+    a=b;
+    b=c;
+    printf("\t%d",c);
+    
+  }
+  getch();
+};
