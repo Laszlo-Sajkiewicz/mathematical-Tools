a952bd801b03233a45cbddb566af2c0833e5e6fd
Laszlo Sajkiewicz <laszlo-94@hotmail.fr>
2015-07-24 09:49:09 +0200

    completed prod.c

---
+++ prod.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+int a;
+int b;
+int res;
+
+printf("\n Enter a number");
+scanf("%d",&a);
+
+printf("\n Enter a second number");
+scanf("%d",&b);
+
+res = a*b;
+printf("\n The result is %d",res);
+}
