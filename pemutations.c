c8af6bb3f618941530cb51bd038e2eabc91dd4cf
Laszlo Sajkiewicz <laszlo-94@hotmail.fr>
2015-07-24 10:28:03 +0200

    completed but not testing

--- pemutations.c
+++ pemutations.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void permut(int *a,int *b)
+{
+  int c =*a;
+  *a=*b;
+  *b=c;
+  }
+int main(){
+  int i=5;
+  int j=6;
+  permut(&i,&j);
+  return 0;
+}
