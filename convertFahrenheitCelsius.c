65c3ef1267014d9e510d0c6d824f4b763abd002f
Laszlo Sajkiewicz <laszlo-94@hotmail.fr>
2015-07-27 08:35:32 +0200

    completed Fahrenheit convert

--- convertFahrenheitCelsius.c
+++ convertFahrenheitCelsius.c
@@ -3,5 +3,11 @@
 
 int main()
 {
-
+  float t_f;//Tepmerature Fahrenheit
+  float t_c;//Temparature Celsius
+printf('Enter the temperature Fahrenheit');
+scanf("%d",&t_f);
+t_c = (t_f - 32) x (5.0 / 9.0);
+printf('The result of the conversion is  %d',t_c);
+retunr EXIT_SUCCES;
 }
